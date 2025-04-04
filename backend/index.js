c6931dccf7ec1feb833dfb2f21ab8ab87f3dbbca
Ranajit Nath <nathranajit6@gmail.com>
2025-04-04 19:02:32 +0530

    backend update

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ app.use('/api/referral', referralRouter);
 app.use('/api/ai', aiRouter);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.status(path.join(__dirname, "../frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
     })
